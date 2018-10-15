ec0b6e4e589dd5fc8946dd2fc75e5dfe5fe8e0dd
Naoki Takezoe <takezoe@apache.org>
2018-10-15 21:00:57 +0900

    Update Scaladoc

--- data/src/main/scala/org/apache/predictionio/data/store/LEventStore.scala
+++ data/src/main/scala/org/apache/predictionio/data/store/LEventStore.scala
@@ -26,7 +26,21 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.Duration
 
 /** This object provides a set of operation to access Event Store
-  * without going through Spark's parallelization
+  * without going through Spark's parallelization.
+  *
+  * Note that blocking methods of this object uses
+  * `scala.concurrent.ExecutionContext.Implicits.global` internally.
+  * Since this is a thread pool which has a number of threads equal to available
+  * processors, parallelism is limited up to the number of processors.
+  *
+  * If this limitation become bottleneck of resource usage, you can increase the
+  * number of threads by declaring following VM options before calling "pio deploy":
+  *
+  * <pre>
+  * export JAVA_OPTS="$JAVA_OPTS \
+  *   -Dscala.concurrent.context.numThreads=1000 \
+  *   -Dscala.concurrent.context.maxThreads=1000"
+  * </pre>
   */
 object LEventStore {
 
@@ -36,19 +50,6 @@ object LEventStore {
 
   /** Reads events of the specified entity. May use this in Algorithm's predict()
     * or Serving logic to have fast event store access.
-    *
-    * Note that this method uses `scala.concurrent.ExecutionContext.Implicits.global`
-    * internally. Since this is a thread pool which has a number of threads equal to
-    * available processors, parallelism is limited up to the number of processors.
-    *
-    * If this limitation become bottleneck of resource usage, you can increase the
-    * number of threads by declaring following VM options before calling "pio deploy":
-    *
-    * <pre>
-    * export JAVA_OPTS="$JAVA_OPTS \
-    *   -Dscala.concurrent.context.numThreads=1000 \
-    *   -Dscala.concurrent.context.maxThreads=1000"
-    * </pre>
     *
     * @param appName return events of this app
     * @param entityType return events of this entityType
@@ -155,19 +156,6 @@ object LEventStore {
 
   /** Reads events generically. If entityType or entityId is not specified, it
     * results in table scan.
-    *
-    * Note that this method uses `scala.concurrent.ExecutionContext.Implicits.global`
-    * internally. Since this is a thread pool which has a number of threads equal to
-    * available processors, parallelism is limited up to the number of processors.
-    *
-    * If this limitation become bottleneck of resource usage, you can increase the
-    * number of threads by declaring following VM options before calling "pio deploy":
-    *
-    * <pre>
-    * export JAVA_OPTS="$JAVA_OPTS \
-    *   -Dscala.concurrent.context.numThreads=1000 \
-    *   -Dscala.concurrent.context.maxThreads=1000"
-    * </pre>
     *
     * @param appName return events of this app
     * @param entityType return events of this entityType
